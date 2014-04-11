3bd929b97e8275f924f20e87e5f95aa8c6bf5b88
paulcarnibella <paulcarnibella@gmail.com>
2014-04-12 08:51:49 +1000

    Added first e2e test, mostly just framing. Still need to implement it via a grunt test:e2e or sommat

---
+++ test/e2e/e2e.js
@@ -0,0 +1,46 @@
+require('colors');
+var chai = require("chai");
+chai.should();
+
+var wd = require('wd');
+
+// enables chai assertion chaining
+describe('mocha spec examples', function() {
+  this.timeout(10000);
+
+  describe("regular mocha usage", function() {
+    var browser;
+
+    // Before everything, setup the webdriver
+    // Note that selenium must be running
+    before(function(done) {
+      browser = wd.promiseChainRemote('http://127.0.0.1:4444/wd/hub');
+      browser
+        .init({browserName:'chrome'})
+        .nodeify(done);  //same as : .then(function() { done(); });
+    });
+
+    beforeEach(function(done) {
+      browser
+        .get("http://localhost:8484/")
+        .nodeify(done);
+    });
+
+    after(function(done) {
+      browser
+        .quit()
+        .nodeify(done);
+    });
+
+    it("should retrieve the page title", function(done) {
+      browser
+        .title()
+        .then(function(title) {
+          title.should.equal("youtube-embedr");
+        })
+        .nodeify(done);
+    });
+
+  });
+
+});
