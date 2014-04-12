0e6b3647fdc224f6f07b570ec0b3d8c73c0228f6
paulcarnibella <paulcarnibella@gmail.com>
2014-04-12 10:57:29 +1000

    Cleaned up the e2e test a bit

--- test/e2e/e2e.js
+++ test/e2e/e2e.js
@@ -4,34 +4,38 @@ var expect = chai.expect;
 
 var wd = require('wd');
 
-// enables chai assertion chaining
-describe('mocha spec examples', function() {
-  this.timeout(10000);
+var testTimeout = 10000; // 10 seconds
+var browser;
 
-  describe("regular mocha usage", function() {
-    var browser;
+// Before anything, setup the session
+before(function(done) {
+  this.timeout(testTimeout);
+  browser = wd.promiseChainRemote('http://127.0.0.1:4444/wd/hub');
+  browser
+    .init({browserName:'chrome'})
+    .nodeify(done);  //same as : .then(function() { done(); });
+});
 
-    // Before everything, setup the webdriver
-    // Note that selenium must be running
-    before(function(done) {
-      browser = wd.promiseChainRemote('http://127.0.0.1:4444/wd/hub');
-      browser
-        .init({browserName:'chrome'})
-        .nodeify(done);  //same as : .then(function() { done(); });
-    });
+// After all, finish the session
+after(function(done) {
+  browser
+    .quit()
+    .nodeify(done);
+});
 
+
+
+/**
+ * Start the Test suite!
+ */
+describe('youtube embedr tests', function() {
+    // Start the session
     beforeEach(function(done) {
       browser
         .get("http://localhost:8484/")
         .nodeify(done);
     });
-
-    after(function(done) {
-      browser
-        .quit()
-        .nodeify(done);
-    });
-
+  
     it("should retrieve the page title", function(done) {
       browser
         .title()
@@ -40,7 +44,4 @@ describe('mocha spec examples', function() {
         })
         .nodeify(done);
     });
-
-  });
-
 });
