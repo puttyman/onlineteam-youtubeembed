78fb970951497646c19c0426930278ecdeefd6d4
paulcarnibella <paulcarnibella@gmail.com>
2014-04-13 15:00:31 +1000

    Fixed basic e2e tests

--- test/e2e/commonSpec.js
+++ test/e2e/commonSpec.js
@@ -56,11 +56,35 @@ describe('Common features', function() {
       .nodeify(done);
   });
 
-  it("should have an image set");
+  it("should have an image set", function(done){
+    browser
+      .elementById(defaultId)
+      .then(function(element) {
+        var bgImg = 'url(http://i.ytimg.com/vi/' + defaultId + '/hqdefault.jpg)';
+        return expect(element.getAttribute('style')).to.eventually.contain('background-image: ' + bgImg);
+      })
+      .nodeify(done);
+  });
 
-  it("should have a non zero height");
+  it("should have a non zero height", function(done) {
+    browser
+      .elementById(defaultId)
+      .then(function(element) {
+        var bgImg = 'url(http://i.ytimg.com/vi/' + defaultId + '/hqdefault.jpg)';
+        return expect(element.getComputedCss('height')).to.eventually.not.equal('0px');
+      })
+      .nodeify(done);
+  });
 
-  it("should have a non zero width");
+  it("should have a non zero width", function(done) {
+    browser
+      .elementById(defaultId)
+      .then(function(element) {
+        var bgImg = 'url(http://i.ytimg.com/vi/' + defaultId + '/hqdefault.jpg)';
+        return expect(element.getComputedCss('width')).to.eventually.not.equal('0px');
+      })
+      .nodeify(done);
+  });
 
   it("should have a play button", function(done) {
     browser
@@ -68,22 +92,6 @@ describe('Common features', function() {
       .then(function(element) {
         return expect(element.elementByClassName('play')).to.eventually.exist;
       })
-      .nodeify(done);
-  });
-
-  it("should have a title", function(done) {
-    browser
-      .elementById(defaultId)
-      .then(function(element) {
-        return element.elementByClassName('text');
-      })
-      .then(function (titleElem) {
-        expect(titleElem).to.exist;
-        return expect(titleElem.text()).to.eventually.be.a('string');
-      })
-      //.fail(function(e) {
-      //  throw new Error("Hit a fail");
-      //})
       .nodeify(done);
   });
 
@@ -99,8 +107,50 @@ describe('Common features', function() {
       .nodeify(done);
   });
 
-  it("should play when you click on the play button too");
+  it("should play when you click on the play button too", function(done) {
+    browser
+      .get("http://localhost:8485/")
+      .elementById(defaultId)
+      .then(function(element) {
+        return element.elementByClassName('play');
+      })
+      .click()
+      .elementsByClassName('ytIframe')
+      .then(function(elements) {
+        expect(elements).to.have.length(1)
+        return expect(elements[0].getAttribute('src')).to.eventually.equal('http://www.youtube.com/embed/' + defaultId + '?autoplay=1');
+      })
+      .nodeify(done);
+  });
 
-  it("should go to the youtube page when the title is clicked");
+  it("should have a title", function(done) {
+    browser
+      .get("http://localhost:8485/")
+      .elementById(defaultId)
+      .then(function(element) {
+        return element.elementByClassName('text');
+      })
+      .then(function (titleElem) {
+        expect(titleElem).to.exist;
+        return expect(titleElem.text()).to.eventually.equal('Dr. Suess\'s Sleep Book');
+      })
+      .nodeify(done);
+  });
+
+  // This isn't quite right, can't get the click to work
+  it("should go to the youtube page when the title is clicked", function(done) {
+    browser 
+      .elementById(defaultId)
+      .then(function(element) {
+        return element.elementByClassName('text');
+      })
+      .then(function(title) {
+        return title.getAttribute('href');
+      })
+      .then(function(href) {
+        expect(href).to.equal('https://www.youtube.com/watch?v=5GedjRxj9EA')
+      })
+      .nodeify(done);
+  });
 
 });
