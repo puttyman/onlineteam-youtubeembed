43f0807976ad0c46c4d75a2996a907d38e5be5b9
paulcarnibella <paulcarnibella@gmail.com>
2014-10-24 10:38:03 +1100

    Removed a lot of the bloat from the unit tests, it was overtesting

--- test/spec/youtube-embedrSpec.js
+++ test/spec/youtube-embedrSpec.js
@@ -58,26 +58,33 @@ describe('My Youtube Embedr Class', function() {
 
   // The individual unit tests for each function
   describe('Testing of functions', function() {
+    var text;
+
     it('should return the id of the element', function() {
       expect(ytE.getId()).toEqual(youtubeId);
     });
+
     it('should show the title when the attr is not set', function() {
       expect(ytE.hideTitle()).toBe(false);
     });
+
     it('should hide the title when the attr is set', function() {
       elem.attr('data-title', 'false');
       expect(ytE.hideTitle()).toBe(true);
     });
+
     it('should be set the background image', function() {
       ytE.setDefaultBgImage();
       expect(elem.css('background-image')).toContain('http://i.ytimg.com');
       expect(elem.css('background-image')).toContain(youtubeId);
     });
+
     it('should be set the correct size when height and width are specified', function() {
       ytE.setSize();
       expect(elem.css('height')).toEqual(height);
       expect(elem.css('width')).toEqual(width);
     });
+
     it('should be set the correct size when only height specified', function() {
       elem.css('width', '0px');
       expect(elem.css('width')).toEqual('0px');
@@ -87,6 +94,7 @@ describe('My Youtube Embedr Class', function() {
       expect(elem.css('height')).toEqual(height);
       expect(elem.css('width')).toEqual(newSize + 'px');
     });
+
     it('should be set the correct size when only width specified', function() {
       elem.css('height', '0px');
       expect(elem.css('height')).toEqual('0px');
@@ -96,16 +104,18 @@ describe('My Youtube Embedr Class', function() {
       expect(elem.css('height')).toEqual(newSize + 'px');
       expect(elem.css('width')).toEqual(width);
     });
+
     it('should add the play button', function() {
       ytE.addPlayButton();
       expect(elem.find('.play')[0]).toBeDefined();
     });
-    var text;
+
     it('should get the title bar', function() {
       spyOn(ytE, 'getTitleBarText').andReturn('Paul Rocks');
       text = ytE.getTitleBarText();
       expect(text).toEqual('Paul Rocks');
     });
+
     it('should set the title bar', function() {
       ytE.addTitleBar(text);
       expect(elem.find('.text').length).toEqual(1);
@@ -113,141 +123,12 @@ describe('My Youtube Embedr Class', function() {
       expect(elem.find('.text').attr('target')).toEqual('_blank');
       expect(elem.find('.text').attr('href')).toContain(youtubeId);
     });
+
     it('should add click behaviour', function() {
       spyOn(ytE, 'setClick');
       ytE.setClick();
       expect(ytE.setClick).toHaveBeenCalled();
     });
   });
-
-
-  // The run function
-  describe('Putting it all together in run', function() {
-
-    beforeEach(function() {
-      spyOn(ytE, 'getTitleBarText').andCallFake(function (req) {
-        var d = $.Deferred();
-        var data = {
-          entry: {
-            title: {
-              $t: 'Paul Rocks'
-            } 
-          }
-        };
-        d.resolve(data);
-        return d.promise();
-      });
-      spyOn(ytE, 'setClick');
-
-      ytE.run();
-    });
-
-    afterEach(function() {
-      delete ytE;
-    });
-
-    it('should have set the size', function() {
-      expect(elem.css('height')).toEqual(height);
-      expect(elem.css('width')).toEqual(width);
-    });
-    it('should have set background the image', function() {
-      expect(elem.css('background-image')).toContain('http://i.ytimg.com');
-      expect(elem.css('background-image')).toContain(youtubeId);
-    });
-    it('should have added the play button', function() {
-      expect(elem.find('.play').length).toEqual(1);
-    });
-    it('should have added the title bar text', function() {
-      expect(elem.find('.text').html()).toEqual('Paul Rocks');
-    });
-    it('should have set the click', function() {
-      expect(ytE.setClick).toHaveBeenCalled();
-    });
-    it('should have set the click even if get failed', function() {
-      expect(ytE.setClick).toHaveBeenCalled();
-    });
-  });
-
-
-  // The run function when data-title = false
-  describe('A run with no title', function() {
-
-    beforeEach(function() {
-      spyOn(ytE, 'getTitleBarText');
-      spyOn(ytE, 'setClick');
-
-      elem.attr('data-title', 'false');
-
-      ytE.run();
-    });
-
-    afterEach(function() {
-      delete ytE;
-    });
-
-    it('should have set the size', function() {
-      expect(elem.css('height')).toEqual(height);
-      expect(elem.css('width')).toEqual(width);
-    });
-    it('should have set background the image', function() {
-      expect(elem.css('background-image')).toContain('http://i.ytimg.com');
-      expect(elem.css('background-image')).toContain(youtubeId);
-    });
-    it('should have added the play button in the right posishhh', function() {
-      expect(elem.find('.play').length).toEqual(1);
-      expect(elem.find('.play').attr('style')).toContain('top: 50%');
-    });
-    it('should have not try and load a title', function() {
-      expect(ytE.getTitleBarText).not.toHaveBeenCalled();
-    });
-    it('should have set the click', function() {
-      expect(ytE.setClick).toHaveBeenCalled();
-    });
-  });
-
-
-  // The run function with a failed get title call
-  describe('Running without a failed title call', function() {
-
-    beforeEach(function() {
-      spyOn(ytE, 'getTitleBarText').andCallFake(function (req) {
-        var d = $.Deferred();
-        d.reject('Fail');
-        return d.promise();
-      });
-      spyOn(ytE, 'setClick');
-      // Mock the console.error function to hide output
-      console.error = function() {
-        return;
-      }
-
-      ytE.run();
-    });
-
-    afterEach(function() {
-      delete ytE;
-    });
-
-    it('should have set the size', function() {
-      expect(elem.css('height')).toEqual(height);
-      expect(elem.css('width')).toEqual(width);
-    });
-    it('should have set background the image', function() {
-      expect(elem.css('background-image')).toContain('http://i.ytimg.com');
-      expect(elem.css('background-image')).toContain(youtubeId);
-    });
-    it('should have added the play button', function() {
-      expect(elem.find('.play').length).toEqual(1);
-    });
-    it('should have added the title bar text', function() {
-      expect(elem.find('.text').length).toEqual(0);
-    });
-    it('should have set the click', function() {
-      expect(ytE.setClick).toHaveBeenCalled();
-    });
-  });
-
-
-  
   
 });
