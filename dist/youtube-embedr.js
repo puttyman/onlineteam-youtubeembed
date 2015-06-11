bd2137b61ba98af114c5c01da8cdf1b367f5b48b
Rowan Parkinson <rowan.parkinson@gmail.com>
2015-06-11 15:29:55 +1000

    Adding lightbox functionality and dataLayer click events for GTM

--- dist/youtube-embedr.js
+++ dist/youtube-embedr.js
@@ -2,10 +2,10 @@
   'use strict';
 
   // Global Strings
-  var iframeString = 
+  var iframeString =
   '<iframe ' +
    'class="ytIframe" ' +
-   'width="%WIDTH%" ' + 
+   'width="%WIDTH%" ' +
    'height="%HEIGHT%" ' +
    'src="//www.youtube.com/embed/%ID%?%PARAMS%" ' +
    'frameborder="0" ' +
@@ -17,6 +17,8 @@
   // no idea how YouTube decides when to autoplay or not :/
   var deviceRegex = /iPhone|iPod|iPad|Android|BlackBerry/i
   var mobileBrowser = window.navigator.userAgent.match(deviceRegex);
+
+  var dataLayer = dataLayer || [];
 
   // The YoutubeEmbedr Class
   var YoutubeEmbedr = function(elem) {
@@ -30,13 +32,14 @@
     // TODO - if - else here for custom/default bg
     that.setDefaultBgImage()
     that.addPlayButton()
+    that.addLightbox()
 
     if (that.elem.data('fluid')) {
       $(window).on('resize', function() {
         that.resize();
       });
     }
-    
+
     var title = that.showTitle();
     if (title === '') {
       that.getTitleBarText()
@@ -84,7 +87,7 @@
 
   YoutubeEmbedr.prototype.setSize = function() {
     var _elem = this.elem
-    
+
     // If no height was set, use the ratio to calculate it
     if (_elem.height() === 0)
       _elem.height(_elem.width() * this.ratio[1]/this.ratio[0] + 'px')
@@ -116,6 +119,14 @@
     this.elem.find('.text').slideDown()
   }
 
+  YoutubeEmbedr.prototype.addLightbox = function() {
+    var that = this
+
+    if (that.elem.attr('data-toggle') === 'lightbox') {
+      that.elem.attr('href', 'https://www.youtube.com/watch?v=' + that.getId() + '')
+    }
+  }
+
   YoutubeEmbedr.prototype.addParams = function(params) {
     // This is a bit tricky, there is always 1 param set,
     // so I'm just kinda adding the rest. Maybe it's simple...
@@ -124,16 +135,30 @@
 
   YoutubeEmbedr.prototype.setClick = function() {
     var that = this
+
     // Sorry Steve W, I'm always gonna use .click!
     this.elem.on('click', function(e) {
       e.stopPropagation()
+
+      dataLayer.push({
+        event: 'videoPlay',
+        eventDetails: {
+          id: $(e.target).attr('id')
+        }
+      });
 
       if (mobileBrowser) {
         window.location = 'https://www.youtube.com/watch?v=' + that.getId();
         return false;
       }
 
-      that.elem.replaceWith(that.iframeGenerator())
+      if (that.elem.attr('data-toggle') === 'lightbox') {
+        e.stopPropagation()
+        that.elem.ekkoLightbox()
+      } else {
+        that.elem.replaceWith(that.iframeGenerator())
+      }
+
     })
     // Prevent the titlebar from playing the video
     this.elem.find('.text').click(function(e) {
